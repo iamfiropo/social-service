0a9f90e340f464c8286608592154c26df871b371
Ropo Olatujoye <ropo@nairabox.com>
2022-01-11 16:46:15 +0100

    Get all posts endpoint add get all posts endpoint re-structure the create post endpoint update all route

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,12 +1,22 @@
 const Post = require('../models/post.model')
 
-module.exports = async (req, res) => {
-  const { title, content } = req.body;
+class PostController {
+  create = async (req, res) => {
+    const { title, content } = req.body;
+  
+    const post = await Post.create({
+      title,
+      content
+    });
+  
+    res.status(201).json(post)
+  }
 
-  const post = await Post.create({
-    title,
-    content
-  });
+  getAll = async (req, res) => {
+    const posts = await Post.find();
 
-  res.json(post)
+    res.status(200).json(posts);
+  }
 }
+
+module.exports = PostController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
-const Post = require('../controllers/post.controller');
+const PostController = require('../controllers/post.controller');
+const postController = new PostController();
 
 module.exports = (router) => {
-  router.post('/', Post);
+  router.post('/posts', postController.create);
+  router.get('/posts', postController.getAll);
 
   return router;
 }
